d0b168e303cb4c34f5ff4cda1bfb3bf07ac0c565
Hugo Roca <hugoroca@belcorp.biz>
2023-11-16 21:02:10 -0500

    module 06 - ABB

---
+++ ABB/ABB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABB
+{
+    internal class ABB
+    {
+        // Unico atributo de la clase ABB
+        public Node raiz;
+
+        public void insert(int valor)
+        {
+            this.insertRecursive(raiz, valor);
+        }
+
+        private Node insertRecursive(Node raiz, int value)
+        {
+            if (raiz == null) return new Node(value);
+
+            if (value < raiz.dato) raiz.izquierda = insertRecursive(raiz.izquierda, value);
+            else if (value > raiz.dato)
+            {
+                raiz.derecha = insertRecursive(raiz.derecha, value);
+            }
+
+            return raiz;
+        }
+
+        public void ShowTree(string prefix = "", bool isLeft = true)
+        {
+            Node node = raiz;
+            if (node != null)
+            {
+                Console.WriteLine($"{prefix} {(isLeft ? "├──" : "└──")} {node.dato}");
+
+                ShowTree(node.izquierda,
+                    $"{prefix} {(isLeft ? "│   " : "    ")}",
+                    true);
+
+                ShowTree(node.derecha,
+                    $"{prefix} {(isLeft ? "│   " : "    ")}",
+                    false);
+            }
+        }
+    }
+}
---
+++ ABB/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ABB aBB = new ABB();
+
+            aBB.insert(10);
+            aBB.insert(5);
+            aBB.insert(23);
+            aBB.insert(1);
+            aBB.insert(40);
+
+            //aBB.ShowTree(aBB.);
+        }
+    }
+}
