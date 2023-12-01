666d40568df4611a7ecd4d609fda4f22d2799469
Hugo Roca <hugoroca@belcorp.biz>
2023-11-30 21:25:44 -0500

    ex final

---
+++ HR_EX_FINAL/Grafo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HR_EX_FINAL
+{
+    public class Grafo
+    {
+        private Dictionary<string, List<string>> _listaUsuariosRedSocial;
+
+        public Grafo()
+        {
+            // Se crea instancia de la misma
+            _listaUsuariosRedSocial = new Dictionary<string, List<string>>();
+        }
+
+        /// <summary>
+        /// Método para cargar todos los usuarios
+        /// </summary>
+        /// <param name="usuarios">Lista de usuario de archivo</param>
+        public void CargarUsuarios(string[] usuarios)
+        {
+            // Recorremos el array string
+            foreach (string usuario in usuarios)
+            {
+                // Insertamos en el grafo con la lista se string vacía
+                _listaUsuariosRedSocial.Add(usuario, new List<string>());
+            }
+        }
+
+        /// <summary>
+        /// Método para enlazar amistad entre dos usuarios
+        /// </summary>
+        /// <param name="persona1">Usuario uno</param>
+        /// <param name="persona2">Usuario dos</param>
+        public void GenerarAmistad(string persona1, string persona2)
+        {
+            // Se verifica si persona2 ya es amigo de persona1 antes de agregarlo
+            if (!_listaUsuariosRedSocial[persona1].Contains(persona2))
+            {
+                _listaUsuariosRedSocial[persona1].Add(persona2);
+            }
+
+            // Se verifica si persona1 ya es amigo de persona2 antes de agregarlo
+            if (!_listaUsuariosRedSocial[persona2].Contains(persona1))
+            {
+                _listaUsuariosRedSocial[persona2].Add(persona1);
+            }
+        }
+
+        /// <summary>
+        /// Método para mostrar las relaciones de amigos de los usuarios
+        /// </summary>
+        public void MostrarAmigos()
+        {
+            foreach (var kvp in _listaUsuariosRedSocial)
+            {
+                Console.WriteLine($"El usuario {kvp.Key} tiene de amigos a: {string.Join(", ", kvp.Value)}");
+            }
+        }
+    }
+}
---
+++ HR_EX_FINAL/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace HR_EX_FINAL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Obtener la carpeta del archivo ejecutable
+            string carpetaEjecutable = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Combinar la carpeta con el nombre del archivo
+            string rutaUsuario = Path.Combine(carpetaEjecutable, "Usuarios.txt");
+            string rutaAmistades = Path.Combine(carpetaEjecutable, "Amistades.txt");
+
+            // Se establece enconding a UTF-8 para obtener las tildes
+            Encoding encoding = Encoding.GetEncoding("ISO-8859-1");
+
+            // Se realiza instancia de la clase Grafo
+            Grafo grafo = new Grafo();
+
+            // Leer usuarios del archivo Usuarios.txt y agregarlos al grafo.
+            string[] usuarios = File.ReadAllLines(rutaUsuario, encoding);
+
+            // Cargar usuarios
+            grafo.CargarUsuarios(usuarios);
+
+            // Cargar realaciones
+            string[] amistades = File.ReadAllLines(rutaAmistades, encoding);
+
+            // Variable auxiliar
+            string persona1 = null;
+
+            // Se recorre la lista de amistades
+            foreach (var lines in amistades)
+            {
+                // Verificar si la línea actual indica el fin de una pareja de amistades
+                if (lines == "***") persona1 = null; // Restablecer la primera persona para la próxima pareja de amistades.
+                else
+                {
+                    // Si la línea no es "***", significa que es un nombre de persona.
+                    // Si la primera persona de la pareja no está establecida, asignar la línea actual a persona1.
+                    if (persona1 == null) persona1 = lines;
+                    else
+                    {
+                        // Si ya tenemos la primera persona, establecer una relación de amistad en el grafo.
+                        grafo.GenerarAmistad(persona1, lines);
+                    }
+                }
+            }
+
+            // Mostramos la lista
+            grafo.MostrarAmigos();
+
+            Console.ReadKey();
+        }
+    }
+}
